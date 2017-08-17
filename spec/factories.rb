f182e7eb2beef04440af342719cff0d6f225844c
Davhot <david.home@mail.ru>
2017-08-18 01:04:51 +0300

    начало работы с factory_girl и добавление database_cleaner в rspec config

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,9 +2,11 @@ FactoryGirl.define do
   factory :main_hot_catch_log do
     log_data            "some message"
     count_log           1
-    id_log_origin_app   1
-    name_app            "my_app"
+    sequence(:id_log_origin_app)  { |n| n }
+    # id_log_origin_app   1
+    sequence(:name_app)  { |n| "my_app#{n}" }
+    # name_app            "my_app"
     from_log            "Rails"
-    status              "ERROR"
+    status              "SERVER_ERROR"
   end
 end
--- spec/models/main_hot_catch_log_spec.rb
+++ spec/models/main_hot_catch_log_spec.rb
@@ -1,9 +1,9 @@
 require "rails_helper"
 
 RSpec.describe MainHotCatchLog, :type => :model do
-  describe "Method find_and_count_log_if_exist" do
+  describe "Method MainHotCatchLog.find_and_count_log_if_exist(id_log, name_app, count_log)" do
 
-    let(:log) { FactoryGirl.create(:main_hot_catch_log) }
+    before(:all) {3.times{ FactoryGirl.create(:main_hot_catch_log) }}
 
     it "args nil" do
       expect(MainHotCatchLog.find_and_count_log_if_exist(nil, nil, nil)).to eq(false)
@@ -13,6 +13,9 @@ RSpec.describe MainHotCatchLog, :type => :model do
       expect(MainHotCatchLog.find_and_count_log_if_exist("", "", "")).to eq(false)
     end
 
+    it "no count_log" do
+      expect(MainHotCatchLog.find_and_count_log_if_exist(1, "my_app1", nil)).to eq(true)
+    end
 
   end
 end
